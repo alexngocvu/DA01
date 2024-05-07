5261595db6baf53a7f4a52bdece808a99e3e28df
Alex Vu <113093913+alexngocvu@users.noreply.github.com>
2024-05-07 23:02:41 +0900

    Update buoi11.sql

--- buoi11.sql
+++ buoi11.sql
@@ -1,6 +1,78 @@
 -- EX 1
-SELECT DISTINCT MIN(replacement cost)
-FROM film;
+SELECT COUNTRY.Continent, FLOOR(AVG(CITY.Population)) AS Average_City_Population
+FROM CITY
+JOIN COUNTRY ON CITY.CountryCode = COUNTRY.Code
+GROUP BY COUNTRY.Continent;
 
 -- EX 2
+SELECT 
+  ROUND(COUNT(texts.email_id)::DECIMAL
+    /COUNT(DISTINCT emails.email_id),2) AS activation_rate
+FROM emails
+LEFT JOIN texts
+  ON emails.email_id = texts.email_id
+  AND texts.signup_action = 'Confirmed';
 
+-- EX 3:
+SELECT 
+  age.age_bucket, 
+  ROUND(100.0 * 
+    SUM(activities.time_spent) FILTER (WHERE activities.activity_type = 'send')/
+      SUM(activities.time_spent),2) AS send_perc, 
+  ROUND(100.0 * 
+    SUM(activities.time_spent) FILTER (WHERE activities.activity_type = 'open')/
+      SUM(activities.time_spent),2) AS open_perc
+FROM activities
+INNER JOIN age_breakdown AS age 
+  ON activities.user_id = age.user_id 
+WHERE activities.activity_type IN ('send', 'open') 
+GROUP BY age.age_bucket;
+
+-- EX 4:
+WITH supercloud AS (
+SELECT 
+  customers.customer_id, 
+  COUNT(DISTINCT products.product_category) as unique_count
+FROM customer_contracts AS customers
+LEFT JOIN products 
+  ON customers.product_id = products.product_id
+GROUP BY customers.customer_id
+)
+
+SELECT customer_id
+FROM supercloud
+WHERE unique_count = (
+  SELECT COUNT(DISTINCT product_category) 
+  FROM products)
+ORDER BY customer_id;
+
+-- EX5: 
+select mgr.employee_id, mgr.name, COUNT(emp.employee_id) as reports_count, ROUND(AVG(emp.age)) as average_age
+from employees emp join employees mgr
+on emp.reports_to = mgr.employee_id
+group by employee_id
+order by employee_id
+
+-- EX 6:
+SELECT 
+    p.product_name, 
+    SUM(o.unit) AS unit
+FROM 
+    Products p
+JOIN 
+    Orders o ON p.product_id = o.product_id
+WHERE 
+    o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY 
+    p.product_id, 
+    p.product_name
+HAVING 
+    SUM(o.unit) >= 100;
+
+-- EX 7:
+SELECT page_id
+FROM pages
+WHERE NOT EXISTS (
+  SELECT page_id
+  FROM page_likes AS likes
+  WHERE likes.page_id = pages.page_id)
